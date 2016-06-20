66ceaac5bf1f4af4f3ba9f6c5918f940d97cd79d
Berm Mac <berm@mac.240>
2016-06-20 21:57:09 +0700

    First build

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # firejs
+docker build -t qoopooh/firejs .
+docker run -it --rm --name node10 -p 80:8080 qoopooh/firejs
---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+
+// Constants
+var PORT = 8080;
+
+// App
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world 1\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
+
