641bdc1a5b2adc1ef7cc9f70c5e03ddae922d760
Berm Quad <berm@simple-different.com>
2016-07-08 10:46:38 +0700

    Add firebase server/client
    - Save session timestamp on firebase
    - Please put service account credentials json on root of project

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var firebase = require('firebase');
+
+firebase.initializeApp({
+  serviceAccount: 'Babel-95cf925002b5.json',
+  databaseURL: 'https://babel-af432.firebaseio.com/',
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  var sessionsRef = firebase.database().ref('sessions');
+  var mySessionRef = sessionsRef.push();
+  mySessionRef.update({ startedAt: firebase.database.ServerValue.TIMESTAMP });
+
   content = {
     title: 'SimDif',
   };
