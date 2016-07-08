85edec5c14160f4642b2d3b4efe471e6aa604c42
Berm Quad <berm@simple-different.com>
2016-07-08 10:33:31 +0700

    Add ga and firebase

---
+++ public/javascripts/firebase_init.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCLVAVlqCBpyfA2f6F0pmQZ1CNN4CMXkms",
+  authDomain: "babel-af432.firebaseapp.com",
+  databaseURL: "https://babel-af432.firebaseio.com",
+  storageBucket: "",
+};
+firebase.initializeApp(config);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  content = {
+    title: 'SimDif',
+  };
+  res.render('index', content);
 });
 
 module.exports = router;
